7b7fdc4d7f8374c6fe6d33b983c339eab936d278
Gundars <Gundars2.Pelns@live.uwe.ac.uk>
2017-09-30 15:25:47 +0100

    Week1_Waves
    Very basic waves

---
+++ AT_Water_Simulation/Assets/Objects/Scripts/WaveController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour {
+
+    public float height;
+    public float waves;
+    public float wSpeed;
+    public float time;
+    public bool on;
+
+	// Use this for initialization
+	void Start () {
+        on = true;
+        //iTween.MoveBy(this.gameObject, iTween.Hash("y", height, "time", time, "looptype", "pingpong", "easetype", iTween.EaseType.easeInOutSine));
+        //iTween.MoveBy(this.gameObject, iTween.Hash("x", waves, "time", wSpeed, "looptype", "pingpong", "easetype", iTween.EaseType.easeInOutSine));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(on == true)
+        {
+            iTween.MoveBy(this.gameObject, iTween.Hash("y", height, "time", time, "looptype", "pingpong", "easetype", iTween.EaseType.easeInOutSine));
+        }
+        else if (on == false)
+        {
+            iTween.MoveBy(this.gameObject, iTween.Hash("x", waves, "time", wSpeed, "looptype", "pingpong", "easetype", iTween.EaseType.easeInOutSine));
+        }
+		
+	}
+}
